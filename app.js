b536f5018a478b06b4b17091778adf4eb7f0d8db
eveconnolly1234 <eve.connolly.1@ucdconnect.ie>
2021-12-05 18:41:13 +0000

    Pause and play icon and video play bug fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = () => {
     const song= document.querySelector('.song');
     const play= document.querySelector('.play');
     const outline= document.querySelector('.moving-outline circle');
-    const video= document.querySelector('vid-container video');
+    const video= document.querySelector('.vid-container video');
 
     //SOUNDS
     const sounds= document.querySelectorAll('.sound-picker button');
@@ -11,28 +11,30 @@ const app = () => {
     //Length of outline
     const outlineLength= outline.getTotalLength();
     //Duration
+    const timeSelect = document.querySelectorAll(".time-select button");
     let fakeDuration=600;
 
     outline.style.strokeDasharray=outlineLength;
     outline.style.strokeDashoffset=outlineLength;
 
     //Play sound
-    play.addEventListener('click', () =>{
-        song.play();
+    play.addEventListener("click", function() {
         checkPlaying(song);
-    })
+      });
 
     //Stop and Play
-    const checkPlaying= song =>{
-        if(song.paused){
+   const checkPlaying = song => {
+        if (song.paused) {
             song.play();
             video.play();
-            play.src='./svg/pause.svg'
-        }else 
-        song.paused();
-        video.paused();
-        play.src='./svg/play.svg'
-    }
+            play.src = "./svg/pause.svg";
+  } else {
+    song.pause();
+    video.pause();
+    play.src = "./svg/play.svg";
+  }
+};
+
 
 }
 
