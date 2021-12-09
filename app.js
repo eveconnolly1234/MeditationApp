b74663dd61051835e068e543d8b70b4a7d87a4db
eveconnolly1234 <eve.connolly.1@ucdconnect.ie>
2021-12-09 22:29:48 +0000

    Enabled buttons to change between rain and beach

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ const app = () => {
     outline.style.strokeDasharray=outlineLength;
     outline.style.strokeDashoffset=outlineLength;
 
+    //Pick different sounds
+    sounds.forEach(sound =>{
+        sound.addEventListener('click', function(){
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        })
+    })
+
     //Play sound
     play.addEventListener("click", function() {
         checkPlaying(song);
